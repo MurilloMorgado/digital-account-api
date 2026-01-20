3212acb8469342a5c1962f53cb42e8c784d10d3f
Murillo Morgado <mumorgado@hotmail.com>
2026-01-20 07:32:48 -0300

    add status controller for health check

---
+++ src/main/java/br/com/morgado/digitalaccount/api/controller/StatusController.java
@@ -0,0 +1,26 @@
+package br.com.morgado.digitalaccount.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller responsible for providing application health/status information.
+ * This endpoint can be used to verify if the API is running and reachable.
+ */
+@RestController
+@RequestMapping("/status")
+public class StatusController {
+
+     /**
+     * Returns the current status of the API.
+     *
+     * @return a simple message indicating that the API is up and running
+     */
+    @GetMapping
+    public ResponseEntity<String> status(){
+        return ResponseEntity.ok("API online");
+    }
+    
+}
