8aecc09b287480c288d24415eaf8614681667bf4
Murillo Morgado <mumorgado@hotmail.com>
2026-02-13 07:21:28 -0300

    Implementing a service to send the authentication email.

--- src/main/java/br/com/morgado/digitalaccount/api/service/utils/EmailService.java
+++ src/main/java/br/com/morgado/digitalaccount/api/service/utils/EmailService.java
@@ -1,7 +1,5 @@
 package br.com.morgado.digitalaccount.api.service.utils;
 
-import java.io.UnsupportedEncodingException;
-
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
@@ -9,32 +7,36 @@ import org.springframework.stereotype.Service;
 
 import br.com.morgado.digitalaccount.api.domain.model.UserModel;
 import br.com.morgado.digitalaccount.api.exception.BusinessRuleException;
-import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
     private final JavaMailSender javaMailSender;
-    private static final String SENDER_EMAIL = "digitalac@email.com";
+    
+    @Value("${spring.mail.username}")
+    private String senderEmail;
+
     private static final String SENDER_NAME = "Digital Account";
 
-    public static final String URL_SITE = "http://localhost:8080/api";
+    public static final String URL_SITE = "http://localhost:8080/api/users";
 
     @Async
-    private void enviarEmail(String userEmail, String title, String msg) {
+    public void sendEmail(String userEmail, String title, String msg) {
         MimeMessage message = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message);
 
         try {
-            helper.setFrom(SENDER_EMAIL, SENDER_NAME);
+            helper.setFrom(senderEmail, SENDER_NAME);
             helper.setTo(userEmail);
             helper.setSubject(title);
             helper.setText(msg, true);
-        } catch (MessagingException | UnsupportedEncodingException e) {
-            throw new BusinessRuleException("Error sending email");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new BusinessRuleException("Error sending email: " + e.getMessage());
         }
 
         javaMailSender.send(message);
@@ -42,17 +44,17 @@ public class EmailService {
 
     public void sendVerificationEmail(UserModel userModel) {
         String title = "Here is your link to verify your email";
-        String msg = gerarConteudoEmail("Hello [[name]],<br>"
+        String msg = generateEmailContent("Hello [[name]],<br>"
                 + "Please click the link below to verify your account:<br>"
                 + "<h3><a href=\"[[URL]]\" target=\"_self\">VERIFY</a></h3>"
                 + "Thank you,<br>"
                 + "Digital Account :).", userModel.getFullName(),
                 URL_SITE + "/verify-account?code=" + userModel.getToken());
 
-        enviarEmail(userModel.getUsername(), title, msg);
+        sendEmail(userModel.getUsername(), title, msg);
     }
 
-    private String gerarConteudoEmail(String template, String name, String url) {
+    private String generateEmailContent(String template, String name, String url) {
         return template.replace("[[name]]", name).replace("[[URL]]", url);
     }
 
